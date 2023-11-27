6956a584da0d5b9d87c103eb73e243f0b768a7d0
alexander <alexandernyaga1999@gmail.com>
2023-11-27 12:42:50 +0300

    Completed assignment

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,77 @@
 def create_an_empty_list():
-    return None
+    return []
+
 
 def create_a_list():
-    return None
+    new_list = [1, "Hello", True, 3.14]
+    return new_list
+
+my_list = create_a_list()
+print(my_list)
+
 
 def add_element_to_end_of_list(l, element):
-    return None
+    l.append(element)
+    return l
+
+my_list = [1, 2, 3]
+result = add_element_to_end_of_list(my_list, 4)
+print(result) 
+
 
 def add_element_to_start_of_list(l, element):
-    return None
+    l.insert(0, element)
+    return l
+
+my_list = [2, 3, 4]
+result = add_element_to_start_of_list(my_list, 1)
+print(result)
 
 def remove_element_from_end_of_list(l):
-    return None
+    if l:
+        l.pop()
+    return l
+
+my_list = [1, 2, 3, 4]
+result = remove_element_from_end_of_list(my_list)
+print(result)
+
 
 def remove_element_from_start_of_list(l):
-    return None
+    if l:
+        l.pop(0)
+    return l
+
+my_list = [1, 2, 3, 4]
+result = remove_element_from_start_of_list(my_list)
+print(result)  
+
 
 def retrieve_first_element_from_list(l):
+    if l:
+        return l[0]
     return None
+
+my_list = [1, 2, 3, 4]
+result = retrieve_first_element_from_list(my_list)
+print(result) 
+
 
 def retrieve_element_from_index(l, index):
+    if 0 <= index < len(l):
+        return l[index]
     return None
 
+my_list = [1, 2, 3, 4]
+result = retrieve_element_from_index(my_list, 2)
+print(result)  
+
+
 def retrieve_last_element_from_list(l):
+    if l:
+        return l[-1]
     return None
+
+my_list = [1, 2, 3, 4]
+result = retrieve_last_element_from_list(my_list)
+print(result)  
